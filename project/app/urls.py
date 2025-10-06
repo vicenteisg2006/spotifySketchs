8da842698e65caea8c912a9de306dbefde68220e
josefaLara03 <j.laraj@udd.cl>
2025-10-06 15:06:14 -0300

    entorno 2 y nombre login en ruta

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.loginPage, name='login'),
+    path('login/', views.loginPage, name='login'),
     path('artist/', views.artistPage, name='artist'),
     path('user/', views.userPage, name='user'),
 ]
