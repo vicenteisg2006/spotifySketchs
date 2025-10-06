36551b0dc31811ed19061025c9149650b64d077b
Vicente Sepulveda <vicente.isg.2006@gmail.com>
2025-10-06 15:23:41 -0300

    login page, arreglo url ''

--- project/app/views.py
+++ project/app/views.py
@@ -1,9 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        if username == "user" and password == "user123":
+            return redirect("user")
+        elif username == "artist" and password == "artist123":
+            return redirect("artist")
+        else:
+            error_msg = "Invalid credentials, please try again."
+            return render(request, "login.html", {"error": error_msg})
+
     return render(request, 'login.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def artistPage(request):
